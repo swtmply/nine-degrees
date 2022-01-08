8514cfe739aba1d2d70f9a890e5a79f3c962e89d
laumgm <lauramarie.manlapaz@tup.edu.ph>
2022-01-08 22:11:53 +0800

    changed links for social acounts

--- components/Ads/HorizontalAd.jsx
+++ components/Ads/HorizontalAd.jsx
@@ -6,7 +6,7 @@ export default function HorizontalAd() {
   return (
     <div className="col-span-full hidden md:grid grid-cols-8 my-16">
       <div className="col-span-4 col-start-3 h-32 bg-yellowwallow relative">
-        <Link href="https://www.facebook.com/atbp.2n">
+        <Link href="https://www.facebook.com/9degreesph">
           <a target="_blank">
             <Image
               src="/assets/ads/Leaderboard-Ad-1.gif"
