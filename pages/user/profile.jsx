eb31e1870b64838239524238f7db3ed5c8968102
laumgm <lauramarie.manlapaz@tup.edu.ph>
2022-01-13 13:10:49 +0800

    added restriction for unauthorized access

--- pages/user/profile.jsx
+++ pages/user/profile.jsx
@@ -2,6 +2,7 @@ import WriterHeader from "@/components/Dashboard/Header/WriterHeader";
 import WriterTable from "@/components/Dashboard/Table/WriterTable";
 import AdminLayout from "@/components/Layouts/AdminLayout";
 import React from "react";
+import { getSession } from "next-auth/react";
 
 export default function Profile() {
   return (
@@ -11,4 +12,21 @@ export default function Profile() {
       <WriterTable />
     </AdminLayout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
