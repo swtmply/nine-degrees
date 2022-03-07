680d7067af10345aab4ae50cebbecc72fbb07e5f
swtmply <delosreyesjohnallen@gmail.com>
2022-03-07 21:16:56 +0800

    added cover story page

--- components/Articles/NineDegrees/NineDegreesArticle.jsx
+++ components/Articles/NineDegrees/NineDegreesArticle.jsx
@@ -1,37 +1,41 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 
-export default function NineDegreesArticle() {
+export default function NineDegreesArticle({ story, index }) {
+  const router = useRouter();
+
   return (
-    <div className="col-span-full min-h-[420px] relative mb-32">
-      <Image src="/assets/cover-story-bg.png" layout="fill" objectFit="cover" />
-      <div className="w-[80%] h-full flex flex-col sm:flex-row justify-center space-x-4">
-        <div className="bg-slate-400 w-[350px] h-64 sm:h-full relative">
-          <Image
-            src="/assets/samples/PUBMAT SAMPLE.jpg"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
+    <div className="col-span-full min-h-[420px] mb-32 bg-[url('/assets/cover-story-bg.png')] bg-cover">
+      <div className="w-[80%] h-full flex flex-col sm:flex-row justify-center space-x-4 z-10">
+        {parseInt(index) !== 8 && (
+          <div className="bg-slate-400 w-[350px] h-64 sm:h-full relative">
+            <Image
+              src={story.images[0]}
+              layout="fill"
+              objectFit="cover"
+              loading="eager"
+            />
+          </div>
+        )}
         <div className="flex flex-col self-end max-w-[600px]">
           <p className="bg-black text-white text-sm px-8 py-1 rounded font-bold max-w-max">
             Cover Story
           </p>
-          <h2 className="font-bold text-3xl mt-8">
-            SOLIDIFYING KPOP-WAVE: 6 rookie groups to check out in 2022
-          </h2>
+          <h2 className="font-bold text-3xl mt-8">{story.title}</h2>
           <p className="font-mono font-bold mt-2">
-            9Degrees
+            9Degrees Team
             <span className="text-gray-400 ml-2">5 hours ago</span>
           </p>
-          <p className="mt-5">
-            Korean popular music, or simply K-pop, has grown undeniable success
-            in the past years. To date, it’s not just in Asia but other parts of
-            the world. This music and entertainment scene became one of the
-            major influences of Hallyu or the Korean Wave, while idol groups
-            such as...
-          </p>
-          <button className="font-bold bg-black text-white px-8 py-2 mt-4 max-w-max">
+          {story.paragraphs[0].split("\n").map((str) => (
+            <p className="mt-2">{str}</p>
+          ))}
+          <button
+            onClick={() => {
+              router.push(`/nine-degrees/${index}`);
+            }}
+            className="font-bold bg-black text-white px-8 py-2 mt-4 max-w-max hover:bg-yellowwallow"
+          >
             Continue Reading &rarr;
           </button>
         </div>
---
+++ pages/nine-degrees/cover-story.jsx
@@ -0,0 +1,67 @@
+import NineDegreesArticle from "@/components/Articles/NineDegrees/NineDegreesArticle";
+import ClientLayout from "@/components/Layouts/ClientLayout";
+import { stories } from "@/lib/cover-stories";
+import React from "react";
+
+export default function CoverStory() {
+  return (
+    <ClientLayout category={"nine-degrees"}>
+      <section className="bg-black col-span-full grid grid-cols-12 py-16">
+        <div className="text-white text-xl space-y-4 col-span-10 col-start-2">
+          <h1 className="text-5xl text-center">
+            <span>
+              OTW: <br />
+            </span>
+            9 individuals onto their highest form yet
+          </h1>
+          <p>
+            In a highway called life, people are forced to hop on a ride-or-die
+            drive towards their greater selves. A trip where they experience
+            different colors of living and dying. A trip into enlightenment.
+            Youth serves as the path where they pursue and experience first
+            blows of change and reality.
+          </p>
+          <p>
+            As vibrant souls, the youth are always on the lookout for what best
+            feeds their hearts and minds. While as curious go-getters, they are
+            not afraid to take risks. Hence, leading them to a series of
+            setbacks, twists and turns, and even failures. This phase in their
+            life is the most crucial of all. Signifying the end of their young
+            beginning and the beginning of more beginnings. Life at this point
+            is eventful. Everything they will experience will shape and carve
+            their paths as individuals.
+          </p>
+          <p>
+            Troubles and difficulties are an inevitable fact of life. This is
+            one of the core lessons of Buddhist teachings on attaining one’s
+            greater self. According to the religion, there are nine levels one
+            must ponder upon to experience the fullest of life. Among the levels
+            are senses of sight, hearing, smell, taste, and touch; reason,
+            subconscious, karma, and wisdom — each of which have their own ways
+            of showing struggle and change. Likewise, many youth have their own
+            stories reflecting these levels of consciousness.
+          </p>
+          <p>
+            Varying people have varying roads to take. But it might be possible
+            that at some point, one might meet another in an intersection of
+            shared experiences, pains, and victories.
+          </p>
+          <p>
+            <span>Words by MJ</span> Catequista, Kim Leal, Angela Guiral, Alfred
+            Esmilla, Ailla Dela Cruz, Khiel Flores, and Mary Grace Paneda
+          </p>
+          <p>
+            <span>Research by MJ</span> Catequista, Kim Leal, Angela Guiral,
+            Alfred Esmilla, Ailla Dela Cruz, Khiel Flores, Gea Adlag, Jaymar
+            Aquino, and Mary Grace Paneda
+          </p>
+        </div>
+      </section>
+      <section className="col-span-full grid grid-cols-12 py-16">
+        {stories.map((story, idx) => (
+          <NineDegreesArticle story={story} index={idx} key={idx} />
+        ))}
+      </section>
+    </ClientLayout>
+  );
+}
